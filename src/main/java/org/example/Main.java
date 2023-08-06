504f10984cd09006deab27d093b61a90e86467d1
AnUser99 <kisamasyanya@gmail.com>
2023-08-06 21:33:47 +0300

    PostConstruct PreDestroy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,11 @@
 package org.example;
 
 import config.ProjectConfig;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         for (String beanName: context.getBeanDefinitionNames()) {
             System.out.println(beanName);
@@ -19,7 +17,8 @@ public class Main {
         System.out.println(parrot1.getName());
 
         Sparrow sparrow1 = context.getBean(Sparrow.class);
-        sparrow1.setName("Jack");
         System.out.println(sparrow1.getName());
+
+        context.close();
     }
 }
--- src/main/java/org/example/Sparrow.java
+++ src/main/java/org/example/Sparrow.java
@@ -1,10 +1,23 @@
 package org.example;
 
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class Sparrow {
     private String name;
+
+    @PostConstruct // выполняется сразу после создания бина
+    public void init() {
+        this.name = "Spar";
+    }
+
+    @PreDestroy // выполняется непосредственно перед закрытием и очисткой контекста
+                // попытка очистить контекст Spring может оказаться неудачной
+    public void destroy() {
+        System.out.println("Sparrow destroyed");
+    }
 
     public String getName() {
         return name;
