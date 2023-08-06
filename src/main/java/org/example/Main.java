c4ec88d0d502dec8d0366903d05ff9bbceffbfb2
AnUser99 <kisamasyanya@gmail.com>
2023-08-06 22:41:09 +0300

    Программное добавление бинов в контекст Spring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,13 @@ package org.example;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        for (String beanName: context.getBeanDefinitionNames()) {
+        for (String beanName : context.getBeanDefinitionNames()) {
             System.out.println(beanName);
         }
 
@@ -18,6 +20,27 @@ public class Main {
 
         Sparrow sparrow1 = context.getBean(Sparrow.class);
         System.out.println(sparrow1.getName());
+
+        // Программное добавление бинов в контекст Spring
+        Parrot x = new Parrot();
+        x.setName("ProgParrot");
+        Supplier<Parrot> parrotSupplier = () -> x;
+        context.registerBean("softwareParrot", Parrot.class, parrotSupplier);
+
+        Parrot parrot2 = context.getBean("softwareParrot", Parrot.class);
+        System.out.println(parrot2.getName());
+
+        Parrot x2 = new Parrot();
+        x2.setName("PrimaryParrot");
+        Supplier<Parrot> parrotSupplier2 = () -> x2;
+        context.registerBean(
+                "parrot2",
+                Parrot.class,
+                parrotSupplier2,
+                bc -> bc.setPrimary(true)); // сделать бин первичным
+
+        Parrot parrot3 = context.getBean(Parrot.class);
+        System.out.println(parrot3.getName());
 
         context.close();
     }
